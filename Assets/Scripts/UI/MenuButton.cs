a4cc9a04077ca75e6135e462352a7260f676142d
Cooper Saye <coopersaye@gmail.com>
2020-06-06 13:21:40 -0700

    Update font.

---
+++ Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void SwitchScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    
+    public void SwitchLanguage(string language)
+    {
+        LanguageManager.currentLanguage = language;
+    }
+}
