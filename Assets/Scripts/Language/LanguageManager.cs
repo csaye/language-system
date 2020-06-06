0461f58f8f57e69e831c3f33559e4f9e8d02c753
Cooper Saye <coopersaye@gmail.com>
2020-06-05 23:38:39 -0700

    Implement text field and language manager framework.

---
+++ Assets/Scripts/Language/LanguageManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageManager
+{
+    
+    public static string current;
+
+    void Start()
+    {
+        current = "LanguageEN";
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/TextField.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextField : MonoBehaviour
+{
+    public string fieldID;
+
+    void Awake()
+    {
+        // GetComponent<TextMeshProGUI>().text = LanguageManager.current.fieldID;
+        GetComponent<TMPro.TextMeshProUGUI>().text = "hi";
+    }
+}
